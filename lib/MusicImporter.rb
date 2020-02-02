a78f2657fb56c6619db76ae19180348380e50abd
George Blackmore <gtblackmore@gmail.com>
2020-02-02 19:31:36 +0000

    Automatically backed up by Learn

--- lib/MusicImporter.rb
+++ lib/MusicImporter.rb
@@ -15,19 +15,3 @@ class MusicImporter
       files.each{|file| Song.create_from_filename(file)}
     end
   end
-
-  module Concerns
-    module Findable
-      def find_by_name(name)
-        all.find{|item| item.name == name}
-      end
-
-      def find_or_create_by_name(name)
-        item = find_by_name(name)
-        if !item
-          item = create(name)
-        end
-        item
-      end
-    end
-  end
