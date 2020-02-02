df1fd71e8ce31b6fd192053159966504eabd354d
George Blackmore <gtblackmore@gmail.com>
2020-02-02 19:35:08 +0000

    Automatically backed up by Learn

--- lib/MusicImporter.rb
+++ lib/MusicImporter.rb
@@ -1,17 +1,17 @@
 class MusicImporter
-    attr_reader :path
+  attr_reader :path
 
-    def initialize(path)
-      @path = path
-    end
-
-    def files()
-      files = Dir["#{@path}/*.mp3"]
-      files.collect{|file| file.slice!("#{path}/")
-      file
-    end
-
-    def import
-      files.each{|file| Song.create_from_filename(file)}
-    end
+  def initialize(path)
+    @path = path
   end
+
+  def files()
+    files = Dir["#{@path}/*.mp3"]
+    files.collect{|file| file.slice!("#{path}/")
+    file
+  end
+
+  def import
+    files.each{|file| Song.create_from_filename(file)}
+  end
+end
