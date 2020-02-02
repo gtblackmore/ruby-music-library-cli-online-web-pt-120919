c588ee47033c16d00136d842549235d1a917dda7
George Blackmore <gtblackmore@gmail.com>
2020-02-02 19:52:48 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,60 +1,48 @@
-require 'concerns.rb'
+require "concerns"
 
 class Artist
-    attr_accessor :name, :songs
+  attr_accessor :name, :songs
 
-    extend Concerns::Findable
-  
-    @@all = []
-  
-    def initialize(name)
-      @name = name
-      @songs = []
-      save
-    end
-  
-    def name
-      @name
-    end
-  
-    def name=(name)
-      @name = name 
-    end
-    
-    def add_song(song_name)
-      if song_name.artist != self
-        song_name.artist = self
-        @songs << song_name
-      end
-    end
-    
-    def genres
-      artist_genres = []
-      
-      @songs.each{|song| artist_genres << song.genre if !@songs.include?(song.genre)}
-      
-      artist_genres
-    end
-      
-    
-    def songs
-      @songs
-    end
-  
-  
-    def self.all
-      @@all
-    end
-  
-    def self.destroy_all
-      @@all.clear
-    end
-  
-    def save
-      @@all << self
-    end
-  
-    def self.create(name)
-      Artist.new(name)
-    end
+  extend Concerns::Findable
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+    save
   end
+
+  def add_song(song)
+    return if song.artist
+    @songs << song
+    song.artist = self
+
+  end
+
+  def genres
+    collection = []
+
+    songs.each do |song|
+      collection << song.genre if !collection.include?(song.genre)
+    end
+
+    collection
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(name)
+    Artist.new(name)
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all = []
+  end
+end 
