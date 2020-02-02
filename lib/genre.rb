ebcaf8975e4210c6765c1244bac71a4bbec9532a
George Blackmore <gtblackmore@gmail.com>
2020-02-02 19:53:17 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,48 +1,46 @@
-require 'concerns.rb'
+require "concerns"
 
-class Genre
-    attr_accessor :name, :songs
+class Genre 
+  attr_accessor :name, :songs
 
-    extend Concerns::Findable
-    
-    @@all = []
-  
-    def initialize(name)
-      @name = name
-      @songs = []
-      save
-    end
-    
-    def songs
-      @songs
-    end
-    
-    def add_song(song_name)
-      if songs.include?(song_name) == false
-        @songs << song_name
-      end
-    end
+  extend Concerns::Findable
 
-    def artist
-      genre_artists = []
+  @@all = []
 
-      songs.each{|song| genre_artists << song.artist if !genre_artists.include?(song.artist)}
-    end
-
-    
-    def self.all
-      @@all
-    end
-    
-    def self.destroy_all
-      @@all.clear
-    end
-    
-    def save
-      @@all << self
-    end
-    
-    def self.create(name)
-      Genre.new(name)
-    end
+  def initialize(name)
+    @name = name
+    @songs = []
+    save
   end
+
+  def add_song(song)
+    @songs << song
+    song.genre = self
+  end
+
+  def artists
+    collection = []
+
+    songs.each do |song|
+      collection << song.artist if !collection.include?(song.artist)
+    end
+
+    collection
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(name)
+    Genre.new(name)
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all = []
+  end
+end 
